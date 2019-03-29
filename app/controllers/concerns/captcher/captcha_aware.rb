c24f702138c67e5e389fd4c20b21db6bfabac621
Ivan Zinovyev <vanyazin@gmail.com>
2019-03-29 23:56:25 +0300

    No need for session in helper methods

--- app/controllers/concerns/captcher/captcha_aware.rb
+++ app/controllers/concerns/captcher/captcha_aware.rb
@@ -2,18 +2,18 @@ module Captcher
   module CaptchaAware
     extend ActiveSupport::Concern
 
-    def load_captcha(session)
+    def load_captcha
       Captcher.captcha_class.restore_or_create(Captcher.config, session)
     end
 
-    def reload_captcha(session)
+    def reload_captcha
       captcha = Captcher.captcha_class.new(config: Captcher.config)
       captcha.store(session)
       captcha
     end
 
-    def confirm_captcha(session, confirmation)
-      captcha = load_captcha(session)
+    def confirm_captcha(confirmation)
+      captcha = load_captcha
       captcha.validate(confirmation)
     end
     alias confirm_captcha? confirm_captcha
